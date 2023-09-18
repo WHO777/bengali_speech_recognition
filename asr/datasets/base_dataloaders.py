975f0ea31340693c679eef0eb8b9ec21fab4ab9c
WHO777 <ziv.ziv.29@gmail.com>
2023-09-18 11:20:28 +0300

    add TextDataLoader

--- asr/datasets/base_dataloaders.py
+++ asr/datasets/base_dataloaders.py
@@ -1,9 +1,21 @@
 from abc import ABCMeta, abstractmethod
+from typing import Optional
 
 import tensorflow as tf
 
 
 class BaseDataLoader(metaclass=ABCMeta):
+
+    @abstractmethod
+    def get_dataset(self) -> tf.data.Dataset:
+        raise NotImplementedError()
+
+
+class TextDataLoader(BaseDataLoader, metaclass=ABCMeta):
+
+    def __init__(self):
+        self.num_to_char: Optional[tf.keras.layers.StringLookup] = None
+        self.char_to_num: Optional[tf.keras.layers.StringLookup] = None
 
     @abstractmethod
     def get_dataset(self) -> tf.data.Dataset:
