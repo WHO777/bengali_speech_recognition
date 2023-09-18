1bbe15b8d7496ca2975951886422650f4ae5c20a
WHO777 <ziv.ziv.29@gmail.com>
2023-09-18 11:22:29 +0300

    add audio dataset implementation

--- asr/datasets/audio_dataset_dataloader.py
+++ asr/datasets/audio_dataset_dataloader.py
@@ -1,14 +1,17 @@
-import pandas as pd
 from pathlib import Path
+
+import pandas as pd
 import tensorflow as tf
+
 from asr.datasets import load_audios_and_labels
 
 
-class AudioDataset(load_audios_and_labels.LoadAudiosAndLabels):
+class AudioDatasetLoader(load_audios_and_labels.LoadAudiosAndLabels):
 
-    def __init__(self, root_dir, *args, **kwargs):
+    def __init__(self, root_dir: str, *args, **kwargs):
         df_path = Path(root_dir) / 'df.csv'
-        assert df_path.is_file(), f'There is not file "df.csv" in {root_dir}'
+        if not df_path.is_file():
+            raise RuntimeError(f'There is not file "df.csv" in {root_dir}')
         df = pd.read_csv(df_path)
 
         def abs_path_fn(path):
@@ -19,7 +22,10 @@ class AudioDataset(load_audios_and_labels.LoadAudiosAndLabels):
         audio_paths = list(df.path.apply(abs_path_fn))
         labels = list(df.text)
 
-        super(AudioDataset, self).__init__(audio_paths=audio_paths, labels=labels, *args, **kwargs)
+        super(AudioDatasetLoader, self).__init__(audio_paths=audio_paths,
+                                                 labels=labels,
+                                                 *args,
+                                                 **kwargs)
 
     def get_dataset(self) -> tf.data.Dataset:
         return super().get_dataset()
