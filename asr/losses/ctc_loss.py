670984e0b894b877186523ad82b71a405a2724f2
WHO777 <ziv.ziv.29@gmail.com>
2023-09-20 17:46:44 +0300

    ctc loss function to class

--- asr/losses/ctc_loss.py
+++ asr/losses/ctc_loss.py
@@ -1,14 +1,18 @@
 import tensorflow as tf
 
 
-def ctc_loss(y_true, y_pred):
-    batch_len = tf.cast(tf.shape(y_true)[0], dtype='int64')
-    input_length = tf.cast(tf.shape(y_pred)[1], dtype='int64')
-    label_length = tf.cast(tf.shape(y_true)[1], dtype='int64')
+class CTCLoss(tf.Module):
 
-    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype='int64')
-    label_length = label_length * tf.ones(shape=(batch_len, 1), dtype='int64')
+    def __call__(self, y_true, y_pred):
+        batch_len = tf.cast(tf.shape(y_true)[0], dtype='int64')
+        input_length = tf.cast(tf.shape(y_pred)[1], dtype='int64')
+        label_length = tf.cast(tf.shape(y_true)[1], dtype='int64')
 
-    loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, input_length,
-                                           label_length)
-    return loss
+        input_length = input_length * tf.ones(shape=(batch_len, 1),
+                                              dtype='int64')
+        label_length = label_length * tf.ones(shape=(batch_len, 1),
+                                              dtype='int64')
+
+        loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, input_length,
+                                               label_length)
+        return loss
