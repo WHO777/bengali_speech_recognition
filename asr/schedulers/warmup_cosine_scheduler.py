a3ebdb2233ed704b0e14d146a50c2be1c208566e
WHO777 <ziv.ziv.29@gmail.com>
2023-09-20 10:59:03 +0300

    add WarmupCosineScheduler

--- asr/schedulers/warmup_cosine_scheduler.py
+++ asr/schedulers/warmup_cosine_scheduler.py
@@ -1,9 +1,9 @@
 import math
+
 import tensorflow as tf
 
 
-class WarmupCosineScheduler(tf.keras.optimizers.schedules.LearningRateSchedule
-                            ):
+class WarmupCosineScheduler(tf.optimizers.schedules.LearningRateSchedule):
 
     def __init__(self,
                  num_warmup_steps,
@@ -19,19 +19,25 @@ class WarmupCosineScheduler(tf.keras.optimizers.schedules.LearningRateSchedule
         self.num_cycles = num_cycles
 
     def __call__(self, step):
-        log_warmup = self.lr_max * 0.10**(float(self.num_warmup_steps - step))
-        pol_warmup = self.lr_max * 2**-(float(self.num_warmup_steps - step))
+        log_warmup = self.lr_max * 0.10**tf.cast(self.num_warmup_steps - step,
+                                                 tf.float32)
+        pol_warmup = self.lr_max * 2**-tf.cast(self.num_warmup_steps - step,
+                                               tf.float32)
         warmup = tf.where(self.warmup_method == 'log', log_warmup, pol_warmup)
-        progress = float(step - self.num_warmup_steps) / float(
-            max(1, self.num_training_steps - self.num_warmup_steps))
-        cos_lr = max(
-                0.0, 0.5 * (1.0 + math.cos(math.pi * float(self.num_cycles) *
-                                           2.0 * progress))) * self.lr_max
+        progress = tf.cast(step - self.num_warmup_steps, tf.float32) / tf.cast(
+            max(1, self.num_training_steps - self.num_warmup_steps),
+            tf.float32)
+        cos_lr = tf.maximum(
+            0.0, 0.5 *
+            (1.0 + tf.cos(math.pi * tf.cast(self.num_cycles, tf.float32) *
+                          2.0 * progress))) * self.lr_max
         return tf.where(step < self.num_warmup_steps, warmup, cos_lr)
 
     def get_config(self):
-        return dict(num_warmup_steps=self.num_warmup_steps,
-                    lr_max=self.lr_max,
-                    num_training_steps=self.num_training_steps,
-                    warmup_method=self.warmup_method,
-                    num_cycles=self.num_cycles)
+        return {
+            'num_warmup_steps': self.num_warmup_steps,
+            'lr_max': self.lr_max,
+            'num_training_steps': self.num_training_steps,
+            'warmup_method': self.warmup_method,
+            'num_cycles': self.num_cycles
+        }
